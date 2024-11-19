1d8964041668df843e2b422a4d145fbb869bffaf
Anudeep Yesupogu <anudeep.yesupogu@valuelabs.com>
2024-11-19 01:37:33 -0800

    made changes

--- iam_role.tf
+++ iam_role.tf
@@ -54,6 +54,20 @@ resource "aws_iam_policy" "process_void_policy" {
         ],
         Resource = "*"
       }
+      # Lambda VPC Permissions
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:CreateNetworkInterface",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DeleteNetworkInterface",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeVpcs",
+          "ec2:DescribeSecurityGroups"
+        ]
+        Resource = "*"
+      }
+
     ]
   })
 }
